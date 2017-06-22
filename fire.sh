174776fde448e97b1e6abac94fd91a700defd0fa
lobo1111 <root@billy.local>
2017-06-22 15:17:55 +0200

    .

--- fire.sh
+++ fire.sh
@@ -3,6 +3,10 @@
 AUTOMATION_DIR=/opt/my-automation
 mkdir -p $AUTOMATION_DIR
 cd $AUTOMATION_DIR
-curl -L https://omnitruck.chef.io/install.sh | sudo bash
+if which chef > /dev/null; then
+  echo "Chef already installed, skipping"
+else
+  curl -L https://omnitruck.chef.io/install.sh | sudo bash
+fi
 chef-solo -j runlist.json --recipe-url https://github.com/lobo1111/my-automation/raw/master/my-automation.tar.gz
 
